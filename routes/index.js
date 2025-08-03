51399a342401b6d0695e784d906c920b16a1e034
nilberth-escobar <nilberrhescobar@mail.com>
2025-08-02 19:56:33 -0600

    swagger json host and schema updated

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.send('API for mananaging students and courses.');
+});
 
 // Route to serve Swagger UI documentation
 router.use("/api-docs", require("./swagger"));
